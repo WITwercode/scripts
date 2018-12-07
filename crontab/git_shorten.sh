fdedcedc7a3172f1162dd33850b4016132765107
bradleyramos <39171556+bradleyramos@users.noreply.github.com>
2018-12-07 16:27:04 -0600

    No useless batch files through smallhub

--- crontab/git_shorten.sh
+++ crontab/git_shorten.sh
@@ -3,6 +3,5 @@ PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 rm -r /Users/admin/smallhub
 mkdir /Users/admin/smallhub
-cp -p /Users/admin/github/*.sh /Users/admin/smallhub
-cp -p /Users/admin/github/*.bat /Users/admin/smallhub
-cp -Rp /Users/admin/github/WITup\ app /Users/admin/smallhub
+cp -vp /Users/admin/github/*.sh /Users/admin/smallhub
+cp -vRp /Users/admin/github/WITup\ app /Users/admin/smallhub
